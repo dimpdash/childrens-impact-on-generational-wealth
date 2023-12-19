e0742c1ff77fa84ddcf76002b4bafe5547e80a1a
Daniel Hawkins <dimpdash@gmail.com>
2023-12-19 18:34:21 +1100

    init

---
+++ src/main.rs
@@ -0,0 +1,291 @@
+#![allow(unused)]
+
+use bevy::prelude::*;
+use textplots::{utils, Chart, Plot, Shape, AxisBuilder};
+use rand::Rng;
+
+//markers
+#[derive(Component)]
+struct Person;
+
+#[derive(Component)]
+struct Property;
+
+//components
+
+#[derive(Component)]
+struct Age(u32);
+
+#[derive(Component)]
+struct Rent {
+    amount: f32,
+}
+
+#[derive(Component)]
+struct OwnedBy {
+    owner: Entity,
+}
+#[derive(Component)]
+struct LivesIn {
+    property: Entity,
+}
+
+#[derive(Component)]
+struct BankAccount {
+    balance: f32
+}
+
+#[derive(Component)]
+struct Job {
+    salary: f32,
+}
+
+#[derive(Bundle)]
+struct PersonBundle {
+    person: Person,
+    bank_account: BankAccount,
+    job: Job,
+    age: Age,
+}
+
+//bundles
+#[derive(Bundle)]
+struct LandlordBundle {
+    person: Person,
+    bank_account: BankAccount,
+    lives_in: LivesIn,
+    job: Job,
+}
+
+#[derive(Bundle)]
+struct TenantBundle {
+    person: Person,
+    bank_account: BankAccount,
+    lives_in: LivesIn,
+    job: Job,
+}
+
+#[derive(Bundle)]
+struct PropertyBundle {
+    property: Property,
+    rent: Rent,
+    owned_by: OwnedBy,
+}
+
+fn populate_landlord_world(mut commands: Commands) {
+    let number_of_renters = 1000;
+    let number_of_home_owners = 100;
+
+    let place_holder_entity = commands.spawn(()).id();
+
+    let mut landlords = Vec::new();
+
+    for _ in 0..number_of_home_owners {
+        let property_land_lord_lives_in = commands.spawn(PropertyBundle {
+            property: Property,
+            rent: Rent { amount: 1000.0 },
+            owned_by: OwnedBy { owner: place_holder_entity },
+        }).id();
+
+        let landlord = commands.spawn(LandlordBundle {
+            person: Person,
+            bank_account: BankAccount { balance: 1000.0 },
+            lives_in: LivesIn { property: property_land_lord_lives_in },
+            job: Job { salary: 1000.0 },
+        }).id();
+
+        // fix the property to have the landlord as the owner
+        commands.entity(property_land_lord_lives_in).insert(OwnedBy { owner: landlord });
+    
+        landlords.push(landlord);
+    }
+
+    // create the properties the landlord owns
+    // run through the landlords giving them a house
+    // until all the houses are owned
+    let mut upto_landlord_index = 0;
+    for _ in 0..number_of_renters {
+        let property = commands.spawn(PropertyBundle {
+            property: Property,
+            rent: Rent { amount: 1000.0 },
+            owned_by: OwnedBy { owner: place_holder_entity },
+        }).id();
+
+        // fix the property to have the landlord as the owner
+        let landlord = landlords[upto_landlord_index];
+        commands.entity(property).insert(OwnedBy { owner: landlord });
+
+        // create the tenant
+        commands.spawn(TenantBundle {
+            person: Person,
+            bank_account: BankAccount { balance: 500.0 },
+            lives_in: LivesIn { property: property },
+            job: Job { salary: 1000.0 },
+        });
+
+        //increment to next landlord
+        upto_landlord_index = (upto_landlord_index + 1) % landlords.len();
+    }
+}
+
+fn populate_person_world(mut commands: Commands) {
+    let number_of_people = 1000;
+    let min_income = 10_000.0;
+    let max_income = 100_000.0;
+    let min_wealth = 10_000.0;
+    let max_wealth = 100_000_000.0;
+    let rng = &mut rand::thread_rng();
+
+    for _ in 0..number_of_people {
+        let income = rng.gen_range(min_income..max_income);
+        let wealth = rng.gen_range(min_wealth..max_wealth);
+        commands.spawn(PersonBundle {
+            person: Person,
+            bank_account: BankAccount { balance: wealth },
+            job: Job { salary: income },
+            age: Age(0),
+        });
+    }
+}
+
+
+fn print_bank_accounts_system(landlords: Query<(&BankAccount), Without<Rent>>, renters: Query<(&BankAccount), With<Rent>>) {
+    // average landlords bank account
+    let landlord_bank_account : f32 = landlords.iter().fold( 0.0, |acc, b| b.balance + acc) / landlords.iter().len() as f32;
+    let renter_bank_account : f32 = renters.iter().fold( 0.0, |acc, b| b.balance + acc) / landlords.iter().len() as f32;
+
+    println!("Landlord bank account: {}", landlord_bank_account);
+    println!("Renter bank account: {}", renter_bank_account);
+}
+
+fn plot_pay_distribution(bank_acounts : Query<(&BankAccount)>) {
+    let data = bank_acounts.iter().map(|b| (0.0, b.balance)).collect::<Vec<_>>();
+    let hist = utils::histogram(&data, -0.1, 1500.0, 16);
+    Chart::new(180, 30, 0.0, 1500.0)
+        .lineplot(
+            &Shape::Bars( &hist)
+            )
+        .nice();
+
+}     
+
+#[derive(Resource)]
+struct IterationCount(usize);
+
+#[derive(Resource)]
+struct PopulationStatistics {
+    number_of_people: Vec<u32>,
+}
+
+fn update_iteration_count(mut iteration_count: ResMut<IterationCount>) {
+    iteration_count.0 += 1;
+}
+
+#[derive(Resource)]
+struct GreetTimer(Timer);
+
+const ITERATIONS : usize = 100;
+
+fn runner(mut app: App) {
+    for _ in 0..ITERATIONS {
+        app.update();
+    }
+}
+
+fn is_at_end(iteration_count : Res<IterationCount>) -> bool {
+    iteration_count.0 + 1 == ITERATIONS
+}
+
+fn age_people(mut people: Query<(&mut Age)>) {
+    for (mut age) in people.iter_mut() {
+        age.0 += 1;
+    }
+}
+
+#[derive(Event)]
+struct DeathEvent(Entity);
+
+fn people_die(mut commands: Commands, people: Query<(Entity, &Age), With<Person>>, mut ev_death: EventWriter<DeathEvent>) {
+    for (entity, age) in people.iter() {
+        if age.0 > 80 {
+            ev_death.send(DeathEvent(entity));
+            commands.entity(entity).despawn();
+        }
+    }
+}
+
+fn people_born(mut commands: Commands, mut ev_death: EventReader<DeathEvent>, people: Query<(&BankAccount)>) {
+    let rng = &mut rand::thread_rng();
+
+    for death_event in ev_death.read() {
+        let bank_account = people.get(death_event.0).unwrap();
+        let wealth = bank_account.balance;
+        // number of children is based of the amount of wealth
+        // richer people have more children
+        let number_of_children = (f32::log10(wealth) / 3.0) as u32;
+        
+        for _ in 0..number_of_children {
+            let income = rng.gen_range(10_000.0..100_000.0);
+            let childs_wealth = wealth / (number_of_children as f32);
+            commands.spawn(PersonBundle {
+                person: Person,
+                bank_account: BankAccount { balance: childs_wealth },
+                job: Job { salary: income },
+                age: Age(0),
+            });
+        }
+    }
+}
+
+fn record_number_of_people(mut population_statistics: ResMut<PopulationStatistics>, people: Query<&Person>) {
+    population_statistics.number_of_people.push(people.iter().len() as u32);
+}
+
+fn plot_age_distribution(mut population_statistics: ResMut<PopulationStatistics>) {
+    let data = population_statistics.number_of_people.iter().enumerate().map(|(i, &n)| (i as f32, n as f32)).collect::<Vec<_>>();
+    println!("data: {:?}", data);
+    // create a line chart 
+    Chart::new(180, 30, 0.0, ITERATIONS as f32)
+        .lineplot(
+            &Shape::Lines(&data)
+        )
+        .nice();
+
+}
+
+pub struct SimPlugin;
+
+impl Plugin for SimPlugin {
+    fn build(&self, app: &mut App) {
+        app
+            .add_event::<DeathEvent>()
+            .insert_resource(IterationCount(0))
+            .insert_resource(PopulationStatistics { number_of_people: Vec::new() })
+            .add_systems(Startup, populate_person_world)
+            
+            .add_systems(Update, people_die)
+            .add_systems(Update, people_born
+                .after(people_die)
+            )
+            .add_systems(Last, age_people)
+            .add_systems(Last, update_iteration_count)
+            .add_systems(Last, record_number_of_people)
+            .add_systems(Update, (
+                plot_pay_distribution.run_if(is_at_end),
+                plot_age_distribution.run_if(is_at_end))
+            )
+            
+
+            ;
+            
+    }
+}
+
+fn main() {
+    App::new()
+        .set_runner(runner)
+        .add_plugins(SimPlugin)
+        .run();
+
+
+}
